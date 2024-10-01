90f1bfe45d30a592668d691c820004b5db6e2b09
Hyrum15 <gleashyr000@alpinesd.org>
2024-10-01 09:54:27 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,24 @@ namespace SpriteKind {
     export const Button = SpriteKind.create()
     export const planet = SpriteKind.create()
 }
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . 1 . . . . . . . . 
+    . . . . . . 1 1 1 . . . . . . . 
+    . . . . . 1 1 1 1 1 . . . . . . 
+    . . . . 1 1 1 1 1 1 1 . . . . . 
+    . . . . 1 1 1 1 1 1 1 . . . . . 
+    . . . . 1 1 1 1 1 1 1 . . . . . 
+    . . . . 1 1 1 1 1 1 1 . . . . . 
+    . . . . 1 1 1 1 1 1 1 . . . . . 
+    . . . . 1 1 1 1 1 1 1 . . . . . 
+    . . . . 1 1 1 1 1 1 1 . . . . . 
+    . . . . 1 1 1 1 1 1 1 . . . . . 
+    . . . . 1 1 1 1 1 1 1 . . . . . 
+    . 1 1 1 1 1 1 1 1 1 1 1 1 1 . . 
+    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
+    . . 1 1 1 1 1 1 1 1 1 1 1 . . . 
+    `, SpriteKind.Player)
 let enemyplanet = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . 6 7 7 6 6 6 6 . . . . . 
@@ -20,3 +38,5 @@ let enemyplanet = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.planet)
+enemyplanet.setScale(8, ScaleAnchor.Middle)
+enemyplanet.setPosition(85, 5)
